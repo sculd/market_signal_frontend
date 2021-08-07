cc6574d254202a5fc7c39e1df4a331f82dd6422f
Hyungjun Lim <junlim@hyungjuimacmini.lan>
2021-08-07 11:30:08 -0400

    update the exec scripts to read the env file

--- exec.ps1
+++ exec.ps1
@@ -1,2 +1,2 @@
-docker build --rm -t auth0-react-01-login .
-docker run --init -p 3000:3000 -p 3001:3001 -it auth0-react-01-login
+docker build --rm -t market-signal-calling-an-api .
+docker run --init -p 3000:3000 -p 3001:3001 --env-file .env.local -it market-signal-calling-an-api
--- exec.sh
+++ exec.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker build -t market-signal-calling-an-api .
-docker run --init -p 3000:3000 -p 3001:3001 -it market-signal-calling-an-api
+docker run --init -p 3000:3000 -p 3001:3001 --env-file .env.local -it market-signal-calling-an-api
